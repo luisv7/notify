ec2b83c2b9514e40023f4073f23b7beb2ecb6084
luisv7 <luisvillarreal_7@icloud.com>
2022-07-12 19:49:37 -0700

    wip: makes initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// require dependencies
+
+// initialize express app
+
+// configure app settings
+
+// mount middlewear
+
+// mount routes
+
+// tell the app to listen for requests from the client
