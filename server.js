739a2d31dd4e4f95a07612648c72ba8e5dbf4d6d
luisv7 <luisvillarreal_7@icloud.com>
2022-07-12 20:04:09 -0700

    add basic code

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // require dependencies
+const express = require("express");
 
 // initialize express app
+const app = express();
 
 // configure app settings
 
 // mount middlewear
 
 // mount routes
+app.get("/", (req, res) => {
+    res.send("<h1>hello World</h1>");
+});
 
 // tell the app to listen for requests from the client
+const port = 3000;
+app.listen(port, () => {
+    console.log(`express is listeting for request on port:${port}`);
+});
