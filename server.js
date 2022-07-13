934a59736f440fc8ec1b927cf590bd70b0e3d5e7
luisv7 <luisvillarreal_7@icloud.com>
2022-07-12 21:48:28 -0700

    wip: configure routes and connection to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 // require dependencies
+require("dotenv").config();
 const express = require("express");
+const notesRouter = require("./controllers/notes");
+const mongoose = require("mongoose");
 
 // initialize express app
 const app = express();
 
 // configure app settings
+const MongoURL = process.env.MONGO_URL
+mongoose.connect(MongoURL);
+mongoose.connection.on("connected", () => {
+    console.log("connected to MongoDB");
+});
 
 // mount middlewear
 
@@ -12,6 +20,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("<h1>hello World</h1>");
 });
+
+app.use("/", notesRouter);
 
 // tell the app to listen for requests from the client
 const port = 3000;
